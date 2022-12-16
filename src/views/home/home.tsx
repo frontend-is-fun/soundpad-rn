ef7c0d5dc896e0e2ebf0e4378b4240270fa1be91
ibegyourpardon <yiziyint@gmail.com>
2022-12-16 15:56:35 +0800

    some a little test

--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -2,15 +2,24 @@ import * as React from 'react';
 
 import {View, Text, Button} from 'react-native';
 
+// sub components import
+import Category from './components/category';
 import styles from './home.module.scss';
 
 type Props = {
   navigation: any;
 };
 
+// 使用 jotai 传递当前分类
+export type Category = {
+  id: number;
+  title: string;
+};
+
 export default function HomeView(props: Props) {
   return (
     <View style={styles.container}>
+      <Category />
       <Button
         title="Go to Test2"
         onPress={() => {
