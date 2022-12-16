75f80033ed217e9d124f6afc0dc48a471edea1a6
ibegyourpardon <yiziyint@gmail.com>
2022-12-16 14:40:39 +0800

    file save but doesn't work style

--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 
-import {SafeAreaView, Text, Button} from 'react-native';
+import {SafeAreaView, View, Text, Button} from 'react-native';
+
+import styles from './home.module.scss';
 
 type Props = {
   navigation: any;
@@ -12,16 +14,18 @@ export default function HomeView(props: Props) {
       style={{
         height: '100%',
       }}>
-      <Button
-        title="Go to Test2"
-        onPress={() => {
-          console.log(1);
-          props.navigation.navigate('Profile');
-        }}
-      />
-      {/*test*/}
-      <Text>sss</Text>
-      <Text>sss</Text>
+      <View style={styles.container}>
+        <Button
+          title="Go to Test2"
+          onPress={() => {
+            console.log(1);
+            props.navigation.navigate('Profile');
+          }}
+        />
+        {/*test*/}
+        <Text>sss</Text>
+        <Text>sss</Text>
+      </View>
     </SafeAreaView>
   );
 }
